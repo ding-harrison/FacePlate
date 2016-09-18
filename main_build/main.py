73f8e38b6b5fda388cbd12f1186aa9e57d0899cd
Harrison Ding <harrisond2015@gmail.com>
2016-09-18 06:51:38 +0000

    Added changing directions

--- main_build/main.py
+++ main_build/main.py
@@ -2,53 +2,61 @@ import cv2
 import urllib
 import os
 import time
+import math
 import pyupm_servo as servo 
 
 FACIAL_XML = "harrcascade_face.xml"
 IMAGE_DIR = "/home/root/FacePlate/images/"
 DEST_IMG = "results/facial.jpg"
-os.system("wget -O /home/root/FacePlate/images/image.jpg http://localhost:9000/?action=snapshot")
-time.sleep(3)
+urllib.urlretrieve(FACIAL_XML)
+WIDTH_CENTER = 320
+CURR_ANGLE = 90
+PERSON_LOC = WIDTH_CENTER
 
-urllib.urlretrieve(IMAGE_DIR + "image.jpg", "image.jpg")
-urllib.urlretrieve("https://raw.githubusercontent.com/Itseez/opencv/master/data/haarcascades/haarcascade_frontalface_alt.xml",FACIAL_XML)
-
-# Open with opencv
-img = cv2.imread("image.jpg")
-# Convert to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# Create the classifier and run the algo
-faceCascade = cv2.CascadeClassifier(FACIAL_XML)
-
-faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5, minSize = (30, 30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
-
-# For each face found in the image, draw a box around it
-for (x, y, w, h) in faces:
-    print("Face found at x: {} y: {} w: {} h: {}".format(x,y, w, h))
-    cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
-cv2.imwrite(DEST_IMG, img)
-
-
-# Create the servo object using D5
-gServo = servo.ES08A(5)
-
-for i in range(0,10): 
-    # Set the servo arm to 0 degrees
-    gServo.setAngle(0)
-    print 'Set angle to 0'
-    time.sleep(1)
-
-    # Set the servo arm to 90 degrees
-    gServo.setAngle(90)
-    print 'Set angle to 90'
-    time.sleep(1)
-
-    # Set the servo arm to 180 degrees
-    gServo.setAngle(180)
-    print 'Set angle to 180'
-    time.sleep(1)
-
+# Reset the servo to 90
+# Create the servo object using D3
+gServo = servo.ES08A(3)
+    gServo.setAngle(CURR_ANGLE)
 # Delete the servo object
 del gServo 
 
+while(True):
+    os.system("wget -O /home/root/FacePlate/images/image.jpg http://localhost:9000/?action=snapshot")
+    time.sleep(1)
+    urllib.urlretrieve(IMAGE_DIR + "image.jpg", "image.jpg")
+    #urllib.urlretrieve("https://raw.githubusercontent.com/Itseez/opencv/master/data/haarcascades/haarcascade_frontalface_alt.xml",FACIAL_XML)
+
+    # Open with opencv
+    img = cv2.imread("image.jpg")
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Create the classifier and run the algo
+    faceCascade = cv2.CascadeClassifier(FACIAL_XML)
+
+    faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5, minSize = (30, 30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+
+    # For each face found in the image, draw a box around it
+    for (x, y, w, h) in faces:
+        print("Face found at x: {} y: {} w: {} h: {}".format(x,y, w, h))
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+        PERSON_LOC = int(x+w/2)
+    cv2.imwrite(DEST_IMG, img)
+
+    """
+        Move person to center of the screen. Depth will be a slight problem; however, 
+        we can use trial and error to find a good ratio based on the width and height
+    """
+    difference = PERSON_LOC - WIDTH_CENTER
+    CURR_ANGLE -= int(math.ceil(difference/50))
+    if(CURR_ANGLE < 0):
+        CURR_ANGLE = 0
+    elif(CURR_ANGLE >= 180):
+        CURR_ANGLE = 180
+    # Create the servo object using D3
+    gServo = servo.ES08A(3)
+        gServo.setAngle(CURR_ANGLE)
+    # Delete the servo object
+    del gServo 
+    
+    time.sleep(3)
